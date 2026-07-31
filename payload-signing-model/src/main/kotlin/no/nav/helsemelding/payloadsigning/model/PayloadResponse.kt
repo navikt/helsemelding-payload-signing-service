8d633de738fc72b24472240123e5eb12cbe8e609
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2026-07-31 15:52:53 +0200

    Lagt til KDoc komentarer i payload-signing-model modulen

--- payload-signing-model/src/main/kotlin/no/nav/helsemelding/payloadsigning/model/PayloadResponse.kt
+++ payload-signing-model/src/main/kotlin/no/nav/helsemelding/payloadsigning/model/PayloadResponse.kt
@@ -2,6 +2,11 @@ package no.nav.helsemelding.payloadsigning.model
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Response containing the processed payload bytes after signing or verification.
+ *
+ * @property bytes the processed payload bytes (required)
+ */
 @Serializable
 data class PayloadResponse(
     val bytes: ByteArray
